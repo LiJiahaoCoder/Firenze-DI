406c39ff78cfb0ef36377b59eb0611ddeee83d76
Jiahao Li <lijiahao_shandong@163.com>
2021-11-14 14:10:46 +0800

    [feat] http method map

---
+++ restful/src/main/java/server/HttpMethodFactory.java
@@ -0,0 +1,21 @@
+package server;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import java.lang.annotation.Annotation;
+import java.util.Map;
+
+public class HttpMethodFactory {
+  private static final Map<Class<? extends Annotation>, HttpMethod> METHOD_MAP = Map.of(
+          GET.class, HttpMethod.GET,
+          PUT.class, HttpMethod.PUT,
+          POST.class, HttpMethod.POST,
+          DELETE.class, HttpMethod.DELETE
+  );
+
+  public static HttpMethod getMethod(Class<? extends Annotation> methodAnnotation) {
+    return METHOD_MAP.get(methodAnnotation);
+  }
+}
