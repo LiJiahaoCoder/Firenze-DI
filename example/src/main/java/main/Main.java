5065e8824c77df5280717fe667afbe9f49c42db3
Jiahao Li <lijiahao_shandong@163.com>
2021-11-20 19:01:57 +0800

    refactor: remove useless entry annotation

--- example/src/main/java/main/Main.java
+++ example/src/main/java/main/Main.java
@@ -1,10 +1,8 @@
 package main;
 
 import com.jiahao.restful.FRestfulApp;
-import com.jiahao.restful.annotation.RestfulEntry;
 
 public class Main {
-  @RestfulEntry
   public static void main(String[] args) throws Exception {
     new FRestfulApp().start(Main.class);
   }
--- restful/src/main/java/com/jiahao/restful/FRestfulApp.java
+++ restful/src/main/java/com/jiahao/restful/FRestfulApp.java
@@ -1,14 +1,6 @@
 package com.jiahao.restful;
 
-import com.jiahao.di.annotation.DIEntry;
-import com.jiahao.restful.annotation.RestfulEntry;
 import com.jiahao.restful.helper.Register;
-
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 public class FRestfulApp {
   private final Register register;
@@ -17,20 +9,9 @@ public class FRestfulApp {
     this.register = new Register();
   }
 
-  @DIEntry
   public void start(Class<?> entry) throws Exception {
     DIContainer.initContainer(entry);
     register(entry);
-    List<Method> methods = Arrays.stream(entry.getMethods())
-            .filter(method -> Objects.nonNull(method.getDeclaredAnnotation(RestfulEntry.class)))
-            .collect(Collectors.toList());
-
-    if (methods.isEmpty()) {
-      throw new RuntimeException("No @RestfulEntry");
-    } else if (methods.size() > 1) {
-      throw new RuntimeException("Can only exist 1 @RestfulEntry");
-    }
-
     createServer().run();
   }
 
