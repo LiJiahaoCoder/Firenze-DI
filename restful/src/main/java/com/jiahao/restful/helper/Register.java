a412a1fa94d047b3cb27e39d6e29fc1deb84b4f7
Jiahao Li <lijiahao_shandong@163.com>
2021-11-21 17:47:35 +0800

    feat: register sub resource

--- restful/src/main/java/com/jiahao/restful/helper/Register.java
+++ restful/src/main/java/com/jiahao/restful/helper/Register.java
@@ -10,23 +10,25 @@ import javax.ws.rs.Path;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.List;
+import java.util.Objects;
 
 public class Register {
 
-  private final static List<Class<? extends Annotation>> httpAnnotations = List.of(GET.class, POST.class);
+  private final static List<Class<? extends Annotation>> httpAnnotations = List.of(GET.class, POST.class, Path.class);
 
   public void register(Class<?> entry) {
-    registerResource(Utils.getAllClasses(entry));
+    registerResource(Utils.getAllClasses(entry), "", false);
   }
 
-  private void registerResource(List<Class<?>> classes) {
+  private void registerResource(List<Class<?>> classes, String basePath, boolean isSubResource) {
     classes.stream()
-            .filter(clz -> clz.isAnnotationPresent(Path.class))
-            .forEach(this::registerMethod);
+            .filter(clz -> isSubResource ||clz.isAnnotationPresent(Path.class))
+            .forEach(clz -> registerMethod(clz, basePath));
   }
 
-  private void registerMethod(Class<?> resource) {
-    String path = resource.getAnnotation(Path.class).value();
+  private void registerMethod(Class<?> resource, String basePath) {
+    Path pathAnnotation = resource.getAnnotation(Path.class);
+    String path = Objects.isNull(pathAnnotation) ? basePath : getSubPath(basePath, pathAnnotation);
     UriTable uriTable = UriTable.getUriTable();
     Method[] methods = resource.getMethods();
 
@@ -37,14 +39,18 @@ public class Register {
 
         if (isHttpMethodAnnotation(annotation)) {
 
-          Quartet<String, Class<? extends Annotation>, Method, Class<?>> quartet = new Quartet<>(
-                  UriResolver.resolve(path),
-                  annotation.annotationType(),
-                  method,
-                  resource
-          );
+          if (Path.class.equals(annotation.annotationType())) {
+            registerResource(List.of(method.getReturnType()), getSubPath(path, method.getAnnotation(Path.class)), true);
+          } else {
+            Quartet<String, Class<? extends Annotation>, Method, Class<?>> quartet = new Quartet<>(
+                    UriResolver.resolve(path),
+                    annotation.annotationType(),
+                    method,
+                    resource
+            );
 
-          uriTable.add(quartet);
+            uriTable.add(quartet);
+          }
 
           break;
 
@@ -58,6 +64,10 @@ public class Register {
 
   private boolean isHttpMethodAnnotation(Annotation annotation) {
     return httpAnnotations.contains(annotation.annotationType());
+  }
+
+  private String getSubPath(String path, Path annotation) {
+    return UriResolver.resolve(path + "/" + UriResolver.resolve(annotation.value()));
   }
 
 }
