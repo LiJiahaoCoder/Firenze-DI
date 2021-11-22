7c7e0691bcbcc2816bc1568179753f5390fe469b
Jiahao Li <lijiahao_shandong@163.com>
2021-11-22 10:12:07 +0800

    feat: extract path parameters

--- restful/src/main/java/com/jiahao/restful/helper/UriHelper.java
+++ restful/src/main/java/com/jiahao/restful/helper/UriHelper.java
@@ -2,6 +2,7 @@ package com.jiahao.restful.helper;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UriHelper {
@@ -12,12 +13,30 @@ public class UriHelper {
     return result;
   }
 
+  private static boolean isPathParam(String target) {
+    return target.matches("\\{\\w+\\}");
+  }
+
+  private static String transform(String target) {
+    return target.replaceAll("\\{\\w+\\}", "(\\\\w+)");
+  }
+
   public static boolean match(String uri, String target) {
-    String regex = target.replaceAll("\\{\\w+\\}", "(\\\\w+)");
+    String regex = transform(target);
     return uri.matches(regex);
   }
 
   public static List<String> parse(String uri, String target) {
-    return List.of("");
+    String[] splitUri = uri.split("/");
+    String[] splitTarget = target.split("/");
+    List<String> result = new ArrayList<>();
+
+    for (int i = 0; i < splitTarget.length; ++i) {
+      if (isPathParam(splitTarget[i])) {
+        result.add(splitUri[i]);
+      }
+    }
+
+    return result;
   }
 }
